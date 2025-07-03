a0836a9ff1ef9e23b749f075a7ba4babd173786e
Nalina1288 <nalinamm.civ@bmsce.ac.in>
2025-07-03 11:25:42 +0530

    Add files via upload

---
+++ USN_Branch.py
@@ -0,0 +1,14 @@
+name = input("Name of the student")
+usn = input("usn")
+branch = input ("branch")
+
+print("Hi, welcome to BMSCE, CV Dept,")
+print(name)
+print("with")
+print (usn)
+print ("belonging to,")
+print(branch)
+
+print(name,"with",usn,"beloning to",branch)
+
+age = int(input("mention your age"))
